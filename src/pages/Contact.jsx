bfede42c49f30ca66d5d9367665a9e440254795d
cfinn7789 <cfinn7789@gmail.com>
2023-11-29 16:12:14 -0500

    installed fontawesome and updated contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,12 +1,27 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faPhone } from '@fortawesome/free-solid-svg-icons';
+
 export default function Contact() {
   return (
     <div className="contact-info">
       <h1 className="title">Contact Me</h1>
         <div className="my-contact">
-          <a href="tel:+689-247-7666">689-247-7666</a>
-          <a href="mailto:cfinn7789@gmail.com">cfinn7789@gmail.com</a>
-          <a href="https://github.com/cfinn7789">My Github</a>
-          <a href="https://www.linkedin.com/in/chelsey-finn-05ab71270/">LinkedIn</a>
+          <a href="tel:+689-247-7666">
+            <FontAwesomeIcon icon={faPhone} />
+          </a>
+          <a href="mailto:cfinn7789@gmail.com">
+            <FontAwesomeIcon icon={faEnvelope} />
+          </a>
+          <a href="https://github.com/cfinn7789">
+            <FontAwesomeIcon icon={faGithub} />
+          </a>
+          <a href="https://www.linkedin.com/in/chelsey-finn-05ab71270/">
+            <FontAwesomeIcon icon={faLinkedin} />
+          </a>
         </div>
     </div>
   );
