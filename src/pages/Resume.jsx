0aa39884724fb19349d75e1c3b3bfb2e2be8bc98
cfinn7789 <cfinn7789@gmail.com>
2023-12-24 11:37:07 -0500

    updated image pathing

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -4,7 +4,7 @@ export default function Resume() {
     return (
       <div className="resume-container">
           <a href={resume} target="_blank" rel="noopener noreferrer">
-                <img src="./images/resume-preview.png" className="image" alt="Resume Preview" />
+                <img src="./images/Resume-preview.jpg" className="image" alt="Resume Preview" />
             </a>
         <h1 className="title">Front-end Proficiencies</h1>
           <p>
